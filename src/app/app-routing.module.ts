df631eb5b827c69768b73327af3e66f2e8289099
Haki-D-Rey <maleisho@gmail.com>
2023-10-23 16:30:29 -0600

    feat: config routing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path: '', // Ruta principal (redirige a 'dashboard')
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./modules/dashboard/dashboard.module').then((m) => m.DashboardModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./modules/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: '**', // Ruta de redirección para rutas no reconocidas
+    redirectTo: 'auth/login', // Puedes redirigir a donde desees
+    pathMatch: 'full',
+  },
+
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,10 @@ import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule } from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { AuthComponent } from './modules/auth/auth.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DashboardComponent, AuthComponent],
   imports: [BrowserModule, AppRoutingModule, ButtonModule, CalendarModule],
   providers: [],
   bootstrap: [AppComponent]
